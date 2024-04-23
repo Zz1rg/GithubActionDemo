2ab43b25f7f581b39b930d8caa79b4f79478c655
Trirath Nondhalee <trirath48@gmail.com>
2024-04-23 23:07:16 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -5,7 +5,7 @@ test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
-test('getMonth returns the long-format month of the year', () => {
+test('getMonth returns month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
     expect( month ).toBe('March');
 });
